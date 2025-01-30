aa2de931c681f3452ee184e4828cc6d9a5484f95
gdoqhuefrygutv <szhu28@riverdale.edu>
2025-01-30 17:48:46 -0500

    Working on 4 and 5

--- src/RectangleArea.java
+++ src/RectangleArea.java
@@ -2,7 +2,7 @@ public class RectangleArea {
     public static void main(String[] args) {
 double width =2.5;
 double height=3.2;
-
+System.out.print("The area of a"+" "+width+" "+"by"+" "+height+" "+"rectangle is"+" "+width*height);
 
     }
 }
